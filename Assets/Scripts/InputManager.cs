a51f5afc3f599d3566e67f47fea7c537fb52dcca
DNAthan <douglasnathan42@gmail.com>
2019-03-17 13:51:44 -0600

    Now detecting which mesh has been clicked.

---
+++ Assets/Scripts/InputManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputManager : MonoBehaviour
+{
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            HandleInput();
+        }
+    }
+
+    void HandleInput()
+    {
+        Ray inputRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if (Physics.Raycast(inputRay, out hit))
+        {
+            //TouchCell(hit.point);
+            print(hit.transform.parent.gameObject.name);
+        }
+    }
+   
+}
